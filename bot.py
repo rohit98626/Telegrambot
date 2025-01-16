b941bdf8df6e241df7f1d2c3dfee47cd78b2c938
rohit <rohitkumarprajapat0@gmail.com>
2025-01-16 13:55:36 +0530

    Initial commit

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-
 from dotenv import load_dotenv
+
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
